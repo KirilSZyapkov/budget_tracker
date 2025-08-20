bbd3cc980750029e1378726f36646e7bbe09eb18
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-08-20 22:08:32 +0300

    feat: implement transformArray function to categorize items by type and structure data for income, expenses, and savings

---
+++ lib/helpers.ts
@@ -0,0 +1,49 @@
+type Item = {
+  id: string;
+  name: string;
+  userId: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  budgetId: string;
+  monthId: string;
+  amount: string;
+  type: string;
+};
+
+type Result = {
+  data: { name: string; amount: string }[];
+  type: string;
+};
+
+export function transformArray(arr: Item[]): Result[] {
+  const map = new Map<string, Result>();
+
+  arr.forEach(item => {
+    if (!map.has(item.type)) {
+      if (item.type === "expense") {
+        map.set(item.type, {
+          data: [],
+          type: item.type
+        });
+      } else if (item.type === "income") {
+        map.set(item.type, {
+          data: [],
+          type: item.type
+        });
+      } else if (item.type === "savings") {
+        map.set(item.type, {
+          data: [],
+          type: item.type
+        });
+      }
+      ;
+    }
+
+    map.get(item.type)!.data.push({
+      name: item.name,
+      amount: item.amount,
+    });
+
+  });
+  return Array.from(map.values());
+}
